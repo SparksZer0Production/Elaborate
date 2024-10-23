d135d41cbfcad815fa4b0a65f7720be423517ea1
sFaucher1020 <fauchersamuel02@gmail.com>
2024-10-22 18:30:40 -0600

    init proj

---
+++ Elaborate/Elaborate/ElaborateApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElaborateApp.swift
+//  Elaborate
+//
+//  Created by Samuel Faucher on 10/22/24.
+//
+
+import SwiftUI
+
+@main
+struct ElaborateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
