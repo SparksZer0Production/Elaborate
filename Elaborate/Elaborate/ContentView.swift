24c5935bf778ab11884b2d1f555c932a7a1b7fd9
sFaucher1020 <fauchersamuel02@gmail.com>
2024-10-23 17:19:22 -0600

    fb dependencies added

--- Elaborate/Elaborate/ContentView.swift
+++ Elaborate/Elaborate/ContentView.swift
@@ -2,13 +2,23 @@
 //  ContentView.swift
 //  Elaborate
 //
-//  Created by Samuel Faucher on 10/22/24.
+//  Created by Samuel Faucher  and Samuel Cross on 10/22/24.
 //
 
 import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    return true
+  }
+}
 
 struct ContentView: View {
     var viewModel = HomeViewModel()
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     
     var body: some View {
         VStack {
